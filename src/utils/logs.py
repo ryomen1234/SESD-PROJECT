f9b5a91fcfe019dd1a627fd4ab7bc97d515ad4a8
harshal_kusalkar <h41497254@gmail.com>
2025-08-03 11:34:07 +0530

    logger module added

---
+++ src/utils/logs.py
@@ -0,0 +1,30 @@
+import logging
+import os
+
+log_path = 'logs'
+os.makedirs(log_path, exist_ok=True)
+
+def get_logger(name: str, file_name: str = "logs.log") -> logging.Logger:
+       
+       log_file_path = os.path.join(log_path, file_name)
+       logger = logging.getLogger(name)
+       logger.setLevel(logging.DEBUG)
+
+       if not logging.handlers:
+              file_logger = logging.FileHandler(log_file_path)
+              console_logger = logging.StreamHandler()
+
+              file_logger.setLevel(logging.DEBUG)
+              console_logger.setLevel(logging.DEBUG)
+
+              formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+              file_logger.setFormatter(formatter)
+              console_logger.setFormatter(formatter)
+
+              logger.addHandler(file_logger)
+              logger.addHandler(console_logger)
+
+       return logger
+              
+
+       
