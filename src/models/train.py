f1c1f18b512d85d832772e2c827564a2a7fdb330
harshal_kusalkar <h41497254@gmail.com>
2025-08-16 12:27:24 +0530

    training pipeline created successfully

--- src/models/train.py
+++ src/models/train.py
@@ -5,6 +5,7 @@ import time
 import pickle
 from sklearn.cluster import KMeans
 from src.utils.logs import get_logger
+from sklearn.metrics import silhouette_score, calinski_harabasz_score
 
 try:
  logger = get_logger(__name__, file_name='train.log')
@@ -21,14 +22,36 @@ def train_model(data: np.ndarray) -> KMeans:
     try:
         logger.info("Model training started.")
         model = KMeans(n_clusters=3, max_iter=100, random_state=42)
-
+        
+        start = time.time()
         model.fit(data)
-        logger.info("Model trained and returned.")
-
+        logger.info(f"Time elapsed to train model: {time.time() -start}.")
+        
         return model
     except Exception as e:
         logger.error(f"Some unexpected error occured: {e} ", exc_info=True)
         raise 
+
+def evalute(model: KMeans, data: np.ndarray, df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    this method evalute the model using 'Silhouette Score' and 'calinski_harabasz_score'.
+    '''
+
+    try: 
+
+        labels = model.labels_
+        logger.info(f"labels predicted/nlabels shape: {labels.shape}")
+        score1 = silhouette_score(data, labels)
+        score2 = calinski_harabasz_score(data, labels)
+        logger.info(f"metrics computed successfully./nsilhouette_score: {score1}/ncalinski_harabasz_score: {score2}")
+        
+        df['labels'] = labels
+        logger.info(f"labels concatenated with dataframe.")
+
+        return df
+    except Exception as e:
+        logger.error("Some unexpected error occured: ",e)
+        raise
 
 
 def main():
@@ -39,7 +62,7 @@ def main():
     save the returned model using pickel.
     in the moddels folder.
     '''
-
+    
     try:
         models_path = r'E:\TY\SESD\SESD-PROJECT\models'
         os.makedirs(models_path, exist_ok=True)
@@ -55,6 +78,15 @@ def main():
         file_path = os.path.join(models_path, 'model_v0.0.pkl')
         logger.info(f"Saving model to: {file_path}")
         
+        df_path = r"E:\TY\SESD\SESD-PROJECT\music-store\music_data.csv"
+        df = pd.read_csv(df_path)
+        df = evalute(model, train_data, df)
+        logger.info("Model evaluat successfull.")
+        
+        music_list_path = r'E:\TY\SESD\SESD-PROJECT\music-store'
+        df.to_csv(os.path.join(music_list_path, 'music_list.csv'), index=False)
+        logger.info(f"music-list saved successfully in: {music_list_path}")
+        
         with open(file_path, 'wb') as f: 
             pickle.dump(model, f, pickle.HIGHEST_PROTOCOL)
         logger.info("Model saved successfully.")
@@ -62,7 +94,6 @@ def main():
     except Exception as e:
         logger.error(f"Unexpected error occurred: {e}", exc_info=True)
         raise
-
 
 if __name__ == "__main__":
     main()
